e96f6c03efdf74a39108f620540153317b951612
wangshujuan.go //邮箱前缀 <wangshujuan.go@bytedance.com>
2022-09-04 18:24:03 +0800

    v2

--- docs/.vitepress/sidebarConfig/index.ts
+++ docs/.vitepress/sidebarConfig/index.ts
@@ -12,7 +12,14 @@ const sidebar = [
       collapsible: true,
       items: [{text: 'index', link: '/configs/'},
       {text: 'one', link: '/configs/one'}]
-    }
+    },
+    {
+        text: 'Golang',
+        collapsible: true,
+        items: [
+        {text: 'golang基本知识', link: '/golang/basic'}
+    ]
+      }
   ]
 
 export default sidebar
